85eaa59c850108c895b317f954e6fe01be4997e7
Manoj Saini <manojsaini513@gmail.com>
2023-07-01 14:53:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 //your JS code here. If required.
-let date = new Date();
-console.log(date);
+let p = document.createElement("p");
+p.id = "timer";
+document.body.append(p);
+
+p.innerHTML = Date();
+
+
+
